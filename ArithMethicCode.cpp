0e3bb6937e5507b922737d42917474bf6658bb86
GK Sir <hellofcode@hell.you>
2021-06-24 21:00:05 +0530

    Thu24Jun202109:00:05PM+0530

---
+++ ArithMethicCode.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int a,b;
+	char ch;
+	cout<<"Enter 2 numbers : ";
+	cin>>a>>ch>>b;
+
+	cout<<"op : "<<ch<<endl;
+	cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+	cout<<a<<" - "<<b<<" = "<<a-b<<endl;
+	cout<<a<<" * "<<b<<" = "<<a*b<<endl;
+	cout<<a<<" / "<<b<<" = "<<a/b<<endl;
+
+return 0;
+}
--- Demo.cpp
+++ Demo.cpp
@@ -1,12 +1,35 @@
 
 #include<iostream>
 using namespace std;
+#define A 100
 int main()
 {
 
-	char name[] = "me";
-
+	char name[5] = "CODE";
 		cout<<"Hello World\t"<<name<<endl;
 
+		#undef A
+		#define A 300
+
+		cout<<" A : "<<A<<endl; // A = 300
+
+		const int a = 30;
+		// a = 50;  // error
+		cout<<"a : "<<a<<endl; // a = 30
+
+		cout<<"hex Data : "<<0X123<<endl;
+		/*
+				16^2	16^1	1
+				--------------------
+				256	    16      1
+				1        2      3
+				-------------------
+				 256 + 32 + 3 = 291
+		*/
+ 		cout<<"new line = \\n  \? ";
+ 		cout<<"hello \"code\" ";
+
+ 		("Hello???")?cout<<"Hello World":cout<<"bye friend";
+ 		
 return 'A';	
 }

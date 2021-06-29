d2fc861d0c1d331c8cc52a062c08f9a7a4ba61a4
GK Sir <hellofcode@hell.you>
2021-06-29 21:12:08 +0530

    Tue29Jun202109:12:08PM+0530

---
+++ AssignmentCode.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int a = 10, b = 3;
+
+	a = a+= b *= 5+5; // b=b*10
+		// a+=30
+	cout<<"code : "<<a<<endl;
+	
+
+}
---
+++ LogicalCode.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	int a=10,b=3;
+
+	
+	if(!NULL && "Ashutosh") // logical AND
+	{
+		cout<<"hello World\n";
+	}
+	else
+	{
+		cout<<"Bye World\n";
+	}
+	////////////
+	if(!(7 || a!=b)) // logical NOT ,logical OR
+	{
+		cout<<"coder....\n";
+	}
+	else
+	{
+		cout<<"noob.......\n";
+	}
+}
